a08fd3bf4b8dbd2728a665825b0ba7db3b892023
Dmitriy Alexandrov <newsmobileads@gmail.com>
2026-01-12 20:00:00 +0100

    Add authorization from docs and refactored code

--- app/api/auth/views.py
+++ app/api/auth/views.py
@@ -1,10 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
 from app.db.dependencies import SessionDep
-from .schemas import UserLoginSchema, UserCreate, JWTToken, UserRead
-from .dependencies import TokenDep
+from .schemas import (
+    UserLoginSchema,
+    UserCreate,
+    JWTToken,
+    UserRead,
+    UserUpdate,
+    OAuthTokenResponse
+)
+from .dependencies import TokenDep, FormDep
 from .service import UserService
 
-auth_router = APIRouter(prefix="/auth", tags=["auth"])
+auth_router = APIRouter(prefix="/api/v1/auth", tags=["auth"])
 
 
 @auth_router.post("/register")
@@ -26,11 +33,30 @@ async def login(
     return await UserService.login(session, user)
 
 
+@auth_router.post("/token", response_model=OAuthTokenResponse)
+async def login_token(
+    session: SessionDep,
+    form: FormDep,
+) -> JWTToken:
+    user = UserLoginSchema(
+        email=form.username,
+        password=form.password,
+    )
+    token = await UserService.login(session, user)
+
+    response = OAuthTokenResponse(
+        access_token=token.token,
+        token_type="bearer"
+    )
+
+    return response
+
+
 @auth_router.get(
     "/me",
     response_model=UserRead,
 )
-async def me(
+async def get_current_user(
     session: SessionDep,
     access_token: TokenDep,
 ):
@@ -38,3 +64,25 @@ async def me(
         session,
         access_token,
     )
+
+
+@auth_router.patch(
+    "/me",
+    response_model=UserRead,
+)
+async def update_current_user(
+    session: SessionDep,
+    access_token: TokenDep,
+    user_schema: UserUpdate,
+):
+    return await UserService.update_user_by_token(
+        session,
+        access_token,
+        user_schema,
+    )
+
+
+@auth_router.post("/logout")
+async def logout():
+    # Удаление refresh_token из httponly-кук.
+    return Response(status_code=200)
