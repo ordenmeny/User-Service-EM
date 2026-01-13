bf41de4402b6738a833ac8967a78ce6f6120ebf4
Dmitriy Alexandrov <newsmobileads@gmail.com>
2026-01-13 18:03:00 +0100

    Changed arch-r of error handlers

--- app/main.py
+++ app/main.py
@@ -1,13 +1,17 @@
 from fastapi import FastAPI
 from app.api.auth.views import auth_router
 import uvicorn
-from app.core.register_handlers import register_exception_handler
+from app.core.register_handlers import register_exception_handlers
+from fastapi.responses import ORJSONResponse
 
 
-app = FastAPI()
+app = FastAPI(
+    default_response_class=ORJSONResponse,
+)
+
 app.include_router(auth_router)
 
-register_exception_handler(app)
+register_exception_handlers(app)
 
 
 if __name__ == "__main__":
